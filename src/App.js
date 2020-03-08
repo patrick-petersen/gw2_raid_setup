53f07f407e677ea5f01291b659e3dbec9e259f99
Patrick <petersen.p@gmx.net>
2020-03-08 20:17:31 +0100

    removed original header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Wing from './raid/Wing.js';
@@ -12,18 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>[Koss] Raid setup</h1>
       </header>
       <main>
           <Wing name={"Wing 1"}>
