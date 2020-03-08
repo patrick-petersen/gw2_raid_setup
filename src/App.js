ca32df320bb66962fbe4bef886d795844caaf278
Patrick <petersen.p@gmx.net>
2020-03-08 20:42:59 +0100

    Added 2nd and 3rd Wing

--- src/App.js
+++ src/App.js
@@ -15,16 +15,92 @@ function App() {
       </header>
       <main>
           <Wing name={"Wing 1"}>
-            <Boss name={"Valeguardian"}>
-                <Setup name={"Center"}>
-                    <Role task={"Tank & Quickness"} profession={"Chronomancer"} player={"Kalell"} />
-                    <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
-                    <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
-                    <Role task={"Seeker Root"} profession={"Soulbeast"} player={"Assa"} />
-                    <Role task={"Seeker Root"} profession={"Soulbeast"} player={"Nevra"} />
-                </Setup>
-            </Boss>
-        </Wing>
+              <Boss name={"Valeguardian"}>
+                  <Setup name={"Center"}>
+                      <Role task={"Tank & Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Seeker Root"} profession={"Soulbeast"} player={"Assa"} />
+                      <Role task={"Seeker Root"} profession={"Soulbeast"} player={"Nevra"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Gorseval"}>
+                  <Setup name={"No Updraft"}>
+                      <Role task={"Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Tank & Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Sabetha"}>
+                  <Setup name={"Normal"}>
+                      <Role task={"Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Kiter"} profession={"Scourge"} player={"Nevra"} />
+                      <Role task={"Cannon 1 & 3"} profession={"Warrior"} player={"David"} />
+                      <Role task={"Cannon 2 & 4"} profession={"?"} player={"Lumi"} />
+                  </Setup>
+              </Boss>
+          </Wing>
+
+          <Wing name={"Wing 2"}>
+              <Boss name={"Slothasor"}>
+                  <Setup name={"Normal"}>
+                      <Role task={"Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Mushroom 1"} profession={"Daredevil"} player={"Assa"} />
+                      <Role task={"Mushroom 2"} profession={"Daredevil"} player={"Nevra"} />
+                      <Role task={"Mushroom 3"} profession={"Weaver"} player={"Fynn"} />
+                      <Role task={"Mushroom 4"} profession={"Weaver"} player={"Alex"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Trio"}>
+                  <Setup name={"Normal"}>
+                      <Role task={"Quickness & Mortar"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Quickness"} profession={"Firebrand"} player={"Yasi"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Saboteur"} profession={"Warrior"} player={"David"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Mathias"}>
+                  <Setup name={"Normal"}>
+                      <Role task={"Buffs"} profession={"Thief"} player={"Assa"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Kalell"} />
+                      <Role task={"Healing & Resurrecting"} profession={"Scourge"} player={"Ely"} />
+                      <Role task={"Reflect"} profession={"Mirage"} player={"Nevra"} />
+                  </Setup>
+              </Boss>
+          </Wing>
+
+          <Wing name={"Wing 3"}>
+              <Boss name={"Escort"}>
+                  <Setup name={"Solo cap"}>
+                      <Role task={"Solo cap"} profession={"Mirage"} player={"Kalell"} />
+                      <Role task={"Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Quickness"} profession={"Firebrand"} player={"Yasi"} />
+                      <Role task={"Warg"} profession={"Warrior"} player={"David"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Keep Construct"}>
+                  <Setup name={"Normal"}>
+                      <Role task={"Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Tank & Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                      <Role task={"Pusher"} profession={"Chronomander"} player={"Fynn"} />
+                  </Setup>
+              </Boss>
+              <Boss name={"Xera"}>
+                  <Setup name={"Mid"}>
+                      <Role task={"Quickness"} profession={"Chronomancer"} player={"Kalell"} />
+                      <Role task={"Tank & Heal & Buffs"} profession={"Tempest"} player={"Schrotty"} />
+                      <Role task={"Alacrity"} profession={"Renegade"} player={"Ely"} />
+                  </Setup>
+              </Boss>
+          </Wing>
       </main>
     </div>
   );
