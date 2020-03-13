777b4403692bbeec6607c72b828e052a724a3711
Patrick <petersen.p@gmx.net>
2020-03-13 11:59:03 +0100

    Added default green background for full setups (as opposed to yellow for backups and red for missing positions)

--- src/raid/Setup.js
+++ src/raid/Setup.js
@@ -3,14 +3,17 @@ import React, {Component} from 'react';
 import './Setup.css';
 
 class Setup extends Component {
+    static setupStates = ["main-setup", "backup-setup", "missing-setup"];
+
     constructor(props) {
         super(props);
         this.state = {
+            setupState: Setup.setupStates[0]
         };
     }
     render() {
         return (
-            <div className={"setup"}>
+            <div className={"setup " + this.state.setupState}>
                 <div className={"setup-name"}>
                     <h3>Setup: {this.props.name}</h3>
                 </div>
