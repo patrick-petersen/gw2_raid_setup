9e8034ace253670dfe346f218baa256742325069
Patrick <petersen.p@gmx.net>
2020-04-19 20:35:26 +0200

    Implemented generic routing

--- src/Setups.js
+++ src/Setups.js
@@ -1,4 +1,10 @@
 import React, {Component} from "react";
+import functions from "./helper/functions";
+import Sidebar from "./settings/Sidebar";
+
+import "./Setups.scss";
+
+//Setups:
 import FullComp from "./Setups/FullComp";
 import Marvin from "./Setups/Marvin";
 import LumiEly from "./Setups/LumiEly";
@@ -6,28 +12,78 @@ import LumiEly from "./Setups/LumiEly";
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Link,
+    useParams
 } from "react-router-dom";
+
+const currentWeek = functions.getWeekNumber();
+
+const weeklySetups = [
+    {
+        week: 17,
+        setup: () => <Marvin/>,
+    }
+];
 
 class Setups extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                <Route exact path="/">
-                    <FullComp />
-                </Route>
-                    <Route path="/marvin">
-                        <Marvin />
+                    {weeklySetups.map((setup, index) => (
+                            // Render more <Route>s with the same paths as
+                            // above, but different components this time.
+                            <Route
+                                key={index}
+                                path={'/'+setup.week}
+                                exact={setup.exact}
+                                children={<GenericSetup id={setup.week}><setup.setup /></GenericSetup>}
+                            />))
+                    }
+                    <Route path="/:id" children={<AutomatedSetup />} />
+                    <Route>
+                        <DefaultSetup />
                     </Route>
-                    <Route path="/1304">
-                        <LumiEly />
-                    </Route>
-                <Route>
-                    <FullComp />
-                </Route>
-            </Switch>
+                </Switch>
             </Router>);
+    }
+}
+function AutomatedSetup() {
+    let { id } = useParams();
+
+    return (
+        <GenericSetup id={id}>
+            <FullComp />
+        </GenericSetup>
+    );
+}
+
+function DefaultSetup() {
+    const id = currentWeek;
+
+    return (
+        <GenericSetup id={id}>
+            <FullComp />
+        </GenericSetup>
+    );
+}
+
+class GenericSetup extends Component {
+    render() {
+        const d = functions.getDateOfISOWeek(this.props.id, 2020);
+        const dayString =  d.getDate()  + "." + (d.getMonth()+1) + ".";
+
+        return (
+            <div className={"body"}>
+                <Sidebar />
+                <div className={"content"}>
+                    <h3>Setup for week: {this.props.id} ({dayString})</h3>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+
     }
 }
 
