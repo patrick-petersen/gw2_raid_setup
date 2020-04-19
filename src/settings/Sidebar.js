a3f5c6529e815b78eecf2b3883e0e19fe60c35dc
Patrick <petersen.p@gmx.net>
2020-04-19 20:34:52 +0200

    Created Sidebar class and style (empty)

---
+++ src/settings/Sidebar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import "./Sidebar.scss";
+
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return <div className={"Sidebar"}>Sidebar</div>
+    }
+}
+
+export default Sidebar;
