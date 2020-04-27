a2b2cbb5ec786f6a4187037689fbccc5ad6385e3
Patrick <petersen.p@gmx.net>
2020-04-27 11:52:49 +0200

    Possibly fixed the "current week" calculation

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -11,7 +11,7 @@ module.exports = {
         const dayNum = d.getUTCDay() || 7;
         d.setUTCDate(d.getUTCDate() + 4 - dayNum);
         const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-        return Math.ceil((((d - yearStart + 60*60*24) / 86400000) + 1)/7) + 1
+        return Math.ceil((((d - yearStart) / 86400000) )/7)
     },
 
     getDateOfISOWeek: function(w, y) {
