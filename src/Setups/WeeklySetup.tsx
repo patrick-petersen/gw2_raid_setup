8d403611b68ef47064906789a3ec69430ff26df1
Patrick <petersen.p@gmx.net>
2021-01-03 19:24:20 +0100

    Added year to weeks for setup identification Added said year to links and references for all setups

--- src/Setups/WeeklySetup.tsx
+++ src/Setups/WeeklySetup.tsx
@@ -4,17 +4,19 @@ import functions from "../helper/functions";
 import GenericSetup from "./GenericSetup";
 
 type WeeklySetupProps = {
-    id: number,
+    year: number,
+    week: number,
 }
 
 class WeeklySetup extends Component<WeeklySetupProps> {
     render() {
-        const week = this.props.id;
-        const d = functions.getDateOfISOWeek(week, 2020);
+        const week = this.props.week;
+        const year = this.props.year;
+        const d = functions.getDateOfISOWeek(week, year);
         const dayString =  d.getDate()  + "." + (d.getMonth()+1) + ".";
         return <GenericSetup>
             <h3>
-                <LinkTo id={week - 1}/> Setup for week: {week} ({dayString}) <LinkTo id={week - 1 + 2}/>
+                <LinkTo year={year} week={week - 1}/> Setup for week: {week}-{year} ({dayString}) <LinkTo year={year} week={week - 1 + 2}/>
             </h3>
             {this.props.children}
         </GenericSetup>;
--- src/helper/functions.tsx
+++ src/helper/functions.tsx
@@ -14,6 +14,12 @@ export default {
         console.log((((d.getTime() - yearStart.getTime()) / 86400000))/7);
         return Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1)/7) + 1
     },
+    getYearNumberOfNextMonday : function() {
+        const d = new Date();
+        const dayNum = ((d.getUTCDay() + 6) % 7 ) || 7;
+        d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+        return d.getFullYear()
+    },
 
     getDateOfISOWeek: function(w : number, y : number) {
         const simple = new Date(y, 0, 1 + (w - 1) * 7);
